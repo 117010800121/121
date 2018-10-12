5efa0595fbf06a91a8560af3c11744ce6449e0a9
117010800121 <43244739+117010800121@users.noreply.github.com>
2018-10-12 09:52:32 +0800

    Add files via upload

---
+++ ex2.4.py
@@ -0,0 +1,13 @@
+import turtle
+turtle.setup(650,350,200,200)
+turtle.penup()
+turtle.fd(-60)
+turtle.pendown()
+turtle.pensize(6)
+turtle.pencolor("green")
+turtle.seth(60)
+turtle.fd(120)
+turtle.seth(-60)
+turtle.fd(120)
+turtle.seth(-180)
+turtle.fd(120)
